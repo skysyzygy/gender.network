00cd629aeebbab596e0d12b86e21fe2b8724a16b
ritughiya <ritughiya@gmail.com>
2022-12-05 20:15:08 -0500

    update random on homepage

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,6 @@
 import Header from "../components/Header"
-import randomItem from 'random-item';
-import unquote from 'unquote';
-
-
-
-
 
 function HomePage() {
-
-  var randomTag1 = randomItem([
-    '<div><a href="/category/midwest">Midwest</a></div>', 
-    '<div><a href="/category/NYC">NYC</a></div>', 
-    '<div><a href="/category/pacific-northwest">Pacific Northwest</a></div>', 
-    '<div><a href="/category/san-francisco">San Francisco</a></div>', 
-    '<div><a href="/category/south">South</a></div>'
-  ]);
-  
-  var randomTag2 = randomItem([
-    '<div><a href="/category/1960s">1960s</a></div>', 
-    '<div><a href="/category/1970s">1970s</a></div>', 
-    '<div><a href="/category/1980s">1980s</a></div>', 
-    '<div><a href="/category/1990s">1990s</a></div>', 
-    '<div><a href="/category/2000s">2000s</a></div>'
-  ]);
-
-  var randomTag3 = randomItem([
-    '<div><a href="/category/Storme-DeLarverie">Storme DeLarverie</a></div>', 
-    '<div><a href="/category/Cockettes">Cockettes</a></div>', 
-    '<div><a href="/category/Power-Exchange">Power Exchange</a></div>'
-  ]);
-
-  var randomTag4 = randomItem([
-    '<div><a href="/category/book">Book</a></div>', 
-    '<div><a href="/category/flyer">Flyer</a></div>', 
-    '<div><a href="/category/Newspaper">Newspaper</a></div>', 
-    '<div><a href="/category/Photo">Photo</a></div>'
-  ]);
 
   var randomTag1 =[
     '<div><a href="/category/midwest">Midwest</a></div>', 
@@ -46,6 +11,30 @@ function HomePage() {
   ]
   var randomFact = Math.floor(Math.random() * randomTag1.length);
 
+  var randomTag2 = randomItem([
+    '<div><a href="/category/1960s">1960s</a></div>', 
+    '<div><a href="/category/1970s">1970s</a></div>', 
+    '<div><a href="/category/1980s">1980s</a></div>', 
+    '<div><a href="/category/1990s">1990s</a></div>', 
+    '<div><a href="/category/2000s">2000s</a></div>'
+  ]);
+
+  var randomFact2 = Math.floor(Math.random() * randomTag2.length);
+
+  var randomTag3 = randomItem([
+    '<div><a href="/category/Storme-DeLarverie">Storme DeLarverie</a></div>', 
+    '<div><a href="/category/Cockettes">Cockettes</a></div>', 
+    '<div><a href="/category/Power-Exchange">Power Exchange</a></div>'
+  ]);
+  var randomFact3 = Math.floor(Math.random() * randomTag3.length);
+
+  var randomTag4 = randomItem([
+    '<div><a href="/category/book">Book</a></div>', 
+    '<div><a href="/category/flyer">Flyer</a></div>', 
+    '<div><a href="/category/Newspaper">Newspaper</a></div>', 
+    '<div><a href="/category/Photo">Photo</a></div>'
+  ]);
+  var randomFact4 = Math.floor(Math.random() * randomTag4.length);
 
     return (
       <div className="homePage">
@@ -62,17 +51,17 @@ function HomePage() {
       suppressHydrationWarning className="tag region" dangerouslySetInnerHTML={{__html: randomTag1[randomFact]}}
     />
     <div
-      suppressHydrationWarning className="tag time" dangerouslySetInnerHTML={{__html: randomTag2}}
+      suppressHydrationWarning className="tag time" dangerouslySetInnerHTML={{__html: randomTag2[randomFact2]}}
     />
 <div
-      suppressHydrationWarning className="tag topic" dangerouslySetInnerHTML={{__html: randomTag3}}
+      suppressHydrationWarning className="tag topic" dangerouslySetInnerHTML={{__html: randomTag3[randomFact3]}}
     />
 
 <div
-      suppressHydrationWarning className="tag type" dangerouslySetInnerHTML={{__html: randomTag4}}
+      suppressHydrationWarning className="tag type" dangerouslySetInnerHTML={{__html: randomTag4[randomFact4]}}
     />
        
-          <div className="tag">...</div>
+          <div className="tag"><Link href="/fullindex">...</Link></div>
 
         </div>
       </div>
