92211f8f85a2cf4e782e8f030ad2f9bb44ecf07d
ritughiya <ritughiya@gmail.com>
2022-12-05 20:44:43 -0500

    about header

--- components/Aboutheader.js
+++ components/Aboutheader.js
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 
 function Aboutheader() {
-  return <Image src="/Aboutheader.png" alt="me" width="326" height="138" />
+  return <Image src="/Aboutheader.png" alt="me" width="217" height="92" />
 }
 
 export default Aboutheader
