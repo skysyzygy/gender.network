3d78aa7464ccc2f3f2165796bb31d9502eb0a381
ritughiya <ritughiya@gmail.com>
2022-12-05 19:46:03 -0500

    clean2

--- components/Checkbox.jsx
+++
@@ -1,8 +0,0 @@
-export default function Checkbox({label, id, getSelectedTags}) {
-    return (
-<label className="">
-    <input type="checkbox" value={id} onChange={e=> getSelectedTags(id)}/><span>{label}</span>
-</label>
-  
-    );
-  }
---
+++ components/ItemHeader.js
@@ -0,0 +1,30 @@
+//ItemHeader.js
+
+/**
+ * @jest-environment jsdom
+ */
+
+ import Link from 'next/link'
+ import { useRouter } from 'next/router'
+ import Image from 'next/image'
+ import Check from '../public/Checkmark.svg';
+
+
+ export default function ItemHeader() {
+    return (
+        <div className="itemheader">
+            <div className="flex">
+            <div className="checkbox"> <span>[</span> <span className="selected1"><Image src={Check} alt="Selected" /></span> <span>]</span> </div>Item View</div>        
+            {/* <div className="flex">
+            <a href="javascript:history.back()">
+                <div className="checkbox2"> 
+                
+                <span>[</span> <span className="">&nbsp;Back to Index&nbsp;</span> <span>]</span> 
+                
+                </div>
+                </a></div>                        */}
+        </div>
+    );
+  }
+  
+ 
