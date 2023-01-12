d1f67ae7f6b8b1756a47e4c5c4e78b71cc08d2da
ritughiya <ritughiya@gmail.com>
2023-01-12 16:12:47 -0500

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import Script from 'next/script'
 import {QueryClientProvider, QueryClient} from 'react-query'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
@@ -9,7 +10,21 @@ const queryClient = new QueryClient()
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
-    
+          <Script id="analytics1"
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-D4R0Z132LX`}
+      />
+  
+      <Script id="analytics2" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+  
+          gtag('config', 'G-D4R0Z132LX');
+        `}
+      </Script>
+
     <QueryClientProvider client={queryClient}>
 
       <Component {...pageProps} />
